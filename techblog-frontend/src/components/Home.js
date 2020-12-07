4f5bbc2bcff10d39af57de03e5ca34c057216aaf
Snehil <snehil.udrhj@gmail.com>
2020-12-07 10:23:50 +0530

    added styles+almost done

---
+++ techblog-frontend/src/components/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div className="blogs">
+                <div className="container">
+                <h1>welcome To TechBlogz!</h1>
+                <p>I'm a full stack web developer, from India.  Here, you will find the most awesome techblogzz!</p>
+            </div>
+            </div>
+            
+        );
+    }
+}
+
+export default Home;
